41e7eed1e496e32c406d8ee348aa32ec57178401
ChTayyabMehmood <chtayyabmehmod@gmail.com>
2026-04-24 22:51:52 +0500

    ep-08

---
+++ Ep-08/app.js
@@ -0,0 +1,20 @@
+// Episode-08 | Deep dive into v8 JS Engine
+/*
+
+Node js is runtime enviromenet; 
+in node js we have v8 Engine and other library lib.uv
+
+there are multiple step, when we gave code to v8 
+Parsing 
+step 1:  lexical analysis 
+mean code convert into tokens: also class tokenization
+
+step 2: Syntax Anlysis 
+token convert into ast abstract syntax tree
+
+can see AST WEBSITE: astexplore.net 
+
+
+
+
+*/
